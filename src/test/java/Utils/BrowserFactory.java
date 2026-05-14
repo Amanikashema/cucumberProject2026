bb9eb662a7141a229431c4377bffdd4537ab3984
Amani Kashema <amanikashema000@gmail.com>
2026-05-14 17:06:19 +0200

    added BroswerFactory

---
+++ src/test/java/Utils/BrowserFactory.java
@@ -0,0 +1,30 @@
+package Utils;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.safari.SafariDriver;
+
+public class BrowserFactory {
+    static WebDriver driver;
+
+    public static WebDriver startBrowser(String browserChoice, String url){
+         if(browserChoice.equalsIgnoreCase("chrome")) {
+             driver = new ChromeDriver();
+         }else if(browserChoice.equalsIgnoreCase("edge")){
+             driver =new EdgeDriver();
+         }else if(browserChoice.equalsIgnoreCase("safari")){
+             driver = new SafariDriver();
+         }else if(browserChoice.equalsIgnoreCase("firefox")){
+             driver = new FirefoxDriver();
+         }else{
+             driver = new InternetExplorerDriver();
+         }
+         driver.manage().window().maximize();
+         driver.get(url);
+        return driver;
+}
+}
