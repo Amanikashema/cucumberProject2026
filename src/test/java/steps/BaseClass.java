e975a81760c17b6d1ee0aedcfd91a624d38c551c
Amani Kashema <amanikashema000@gmail.com>
2026-06-01 18:16:31 +0200

    Updated steps Definition

--- src/test/java/steps/BaseClass.java
+++ src/test/java/steps/BaseClass.java
@@ -1,14 +1,22 @@
 package steps;
 
+import Pages.HomePage;
+import Pages.LoginPage;
+import Utils.BrowserFactory;
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.PageFactory;
 
 public class BaseClass {
-    WebDriver driver;
+    static WebDriver driver;
 
     @Before
     public void setup(){
+        BrowserFactory browserFactory =  new BrowserFactory();
+        driver = browserFactory.startBrowser("chrome", "https://ndosisimplifiedautomation.vercel.app/" );
+        HomePage homePage = PageFactory.initElements(driver, HomePage.class);
+        LoginPage loginPage = PageFactory.initElements(driver, LoginPage.class);
 
     }
 
--- src/test/java/steps/stepsDefinition.java
+++ src/test/java/steps/stepsDefinition.java
@@ -1,22 +1,19 @@
 package steps;
 
+import Pages.LoginPage;
 import io.cucumber.java.en.*;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 
-public class stepsDefinition {
+public class stepsDefinition extends BaseClass {
 
     WebDriver driver;
 
     @Given("i am on the login page")
     public void i_am_on_the_login_page() {
-        driver = new ChromeDriver();
-        driver.get("https://ndosisimplifiedautomation.vercel.app/");
-        driver.manage().window().maximize();
-        driver.findElement(By.xpath("/html/body/div/div/nav/div[1]/div[3]/button")).click();
-        Assert.assertTrue(driver.findElement(By.xpath("//*[@id=\"login-heading\"]/span")).isDisplayed(), "Access Learning Materials");
+        LoginPage.class
     }
 
     @Given("I enter email (.*)$")
